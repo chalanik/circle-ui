7c52e123b81d8ba95dc15fb713f6ea7d3b273dcc
nehasinha88 <neha.sinha188@gmail.com>
2022-02-17 12:26:35 +0000

    Adding content for user info page

--- src/Pages/UserInfo/UserInfo.js
+++ src/Pages/UserInfo/UserInfo.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Button } from '@material-ui/core';
+import { Link } from 'react-router-dom';
 import Header from '../../Layout/Header';
 import './UserInfo.css';
 
@@ -10,13 +12,30 @@ function UserInfo() {
                 <h2 className="user-info-title">Let's get some of your basic info</h2>
                 <div className="user-info-form">
                     <div className="user-info-form-items">
-                        <div className="user-info-form-label"><label>Your name</label></div>
+                        <div><label className="user-info-form-label">Your name</label></div>
                         <input type="text" className="user-info-form-text" />
                     </div>
                     <div className="user-info-form-items">
-                        <div className="user-info-form-label"><label>Your zip code</label></div>
+                        <div><label className="user-info-form-label">Your zip code</label></div>
                         <input type="text" className="user-info-form-text" />
                     </div>
+                </div>
+                <div className="posts-container">
+                    Here is how your posts will appear
+                </div>
+                <div className="sample-post">
+                    <div className="post-user-info">
+                        <div className="post-username">Name</div>
+                        <div className="post-time">Today 7 pm</div>
+                    </div>
+                    <div className="post-content">
+                        I'm so excited to join circle
+                    </div>
+                </div>
+                <div className="continue-button-container">
+                    <Link className="get-started-link" to="/">
+                        <Button color="primary" variant="contained">continue</Button>
+                    </Link>
                 </div>
             </div>
         </>
