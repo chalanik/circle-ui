42656823dcff4649cebe20c8ac7c05f83d98d277
nehasinha88 <neha.sinha188@gmail.com>
2022-02-25 13:26:20 +0000

    removing not used imports

--- src/Layout/Post/Post.js
+++ src/Layout/Post/Post.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Card from "@mui/material/Card";
-import CardActionArea from "@mui/material/CardActionArea";
 import CardContent from "@mui/material/CardContent";
 import "../../styles.css";
 import CommentIcon from "@mui/icons-material/Comment";
